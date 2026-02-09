4aaf102348c799843a6a0fb453c31329a87470ee
nikrenxd <nikrenc1101@gmail.com>
2026-02-09 05:02:14 +0200

    create permission to check if user can create new comments under transaction

---
+++ src/cash_flow/apps/comments/permissions.py
@@ -0,0 +1,16 @@
+from rest_framework import permissions
+from rest_framework.exceptions import NotFound
+
+from src.cash_flow.apps.transactions.exceptions import TransactionObjectDoesNotExist
+from src.cash_flow.apps.transactions.selectors import TransactionSelector
+
+
+class IsAllowedAddCommentsToTransaction(permissions.BasePermission):
+    def has_permission(self, request, view) -> bool | None:
+        try:
+            transaction = TransactionSelector().get_transaction(
+                transaction_id=view.kwargs.get("transaction_id")
+            )
+            return request.user.id == transaction.user.id
+        except TransactionObjectDoesNotExist:
+            raise NotFound
