8ab94f8dae889c5d051668c28311f3c6a204bd16
nikrenxd <nikrenc1101@gmail.com>
2026-02-19 05:32:24 +0200

    change comments permission to return false if path parameter is none

--- src/cash_flow/apps/comments/permissions.py
+++ src/cash_flow/apps/comments/permissions.py
@@ -7,6 +7,9 @@ from src.cash_flow.apps.transactions.selectors import TransactionSelector
 
 class IsAllowedAddCommentsToTransaction(permissions.BasePermission):
     def has_permission(self, request, view) -> bool | None:
+        if view.kwargs.get("transaction_id") is None:
+            return False
+
         try:
             transaction = TransactionSelector().get_transaction(
                 transaction_id=view.kwargs.get("transaction_id")
