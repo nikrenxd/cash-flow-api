28f8d77d5c9edd015f4ffd20f7434a5181c9e2e8
nikrenxd <nikrenc1101@gmail.com>
2026-05-26 21:41:34 +0300

    create new exceptions for comments

--- src/cash_flow/apps/comments/exceptions.py
+++ src/cash_flow/apps/comments/exceptions.py
@@ -1,4 +1,19 @@
+from rest_framework import status
+from rest_framework.exceptions import APIException
+
 from src.cash_flow.common.exceptions import ObjectDoesNotExist
+
+
+class CommentActionFailed(Exception):
+    pass
+
+
+class CommentCreationFailed(APIException):
+    status_code = status.HTTP_400_BAD_REQUEST
+
+
+class CommentUpdateFailed(CommentCreationFailed):
+    pass
 
 
 class CommentObjectDoesNotExist(ObjectDoesNotExist):
