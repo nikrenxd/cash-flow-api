acdd7308651f3ee63924dd1d1d97d55cd4daafa6
nikrenxd <nikrenc1101@gmail.com>
2026-06-27 23:26:01 +0300

    add support to create transactions with statuses

--- cash_flow/apps/transactions/api/views.py
+++ cash_flow/apps/transactions/api/views.py
@@ -2,12 +2,20 @@ import logging
 
 from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets
+from rest_framework.exceptions import NotFound
 from rest_framework.permissions import IsAuthenticated
 
+from cash_flow.apps.statuses.exceptions import StatusObjectDoesNotExist
 from cash_flow.apps.transactions.api.serializers import (
     TransactionCreateSerializer,
+    TransactionDetailSerializer,
     TransactionSerializer,
     TransactionUpdateSerializer,
+)
+from cash_flow.apps.transactions.exceptions import (
+    TransactionBadRequest,
+    TransactionCreationError,
+    TransactionUpdateError,
 )
 from cash_flow.apps.transactions.selectors import TransactionSelector
 from cash_flow.apps.transactions.services import TransactionService
@@ -23,7 +31,7 @@ class TransactionViewSet(viewsets.ModelViewSet):
     http_method_names = ["get", "post", "patch", "delete"]
 
     def get_queryset(self):
-        if self.action == "list":
+        if self.action == "retrieve":
             return TransactionSelector().list_transactions_comments(
                 self.request.user.id,
             )
@@ -31,6 +39,8 @@ class TransactionViewSet(viewsets.ModelViewSet):
 
     def get_serializer_class(self):
         match self.action:
+            case "retrieve":
+                return TransactionDetailSerializer
             case "create":
                 return TransactionCreateSerializer
             case "partial_update":
@@ -43,12 +53,24 @@ class TransactionViewSet(viewsets.ModelViewSet):
         user_id = self.request.user.id
         data["user_id"] = user_id
 
-        serializer.instance = TransactionService().create_transaction(**data)
+        try:
+            new_transaction = TransactionService().create_transaction(**data)
+            serializer.instance = new_transaction
+        except TransactionCreationError as e:
+            raise TransactionBadRequest from e
+        except StatusObjectDoesNotExist as e:
+            raise NotFound("Status not found") from e
 
     def perform_update(self, serializer):
         data = serializer.validated_data
         transaction = serializer.instance
-        serializer.instance = TransactionService().update_transaction(
-            transaction,
-            **data,
-        )
+
+        try:
+            serializer.instance = TransactionService().update_transaction(
+                transaction,
+                **data,
+            )
+        except TransactionUpdateError as e:
+            raise TransactionBadRequest from e
+        except StatusObjectDoesNotExist as e:
+            raise NotFound("Status not found") from e
