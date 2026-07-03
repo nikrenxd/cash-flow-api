1b46f6d68309bf528879c3dcc2006dd6aa7ea6b0
nikrenxd <nikrenc1101@gmail.com>
2026-07-03 18:06:58 +0300

    create categories app

---
+++ cash_flow/apps/categories/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+from cash_flow.common.models import BaseModel
+
+
+class Category(BaseModel):
+    name = models.CharField(max_length=125, unique=True)
+
+    transaction_type = models.ForeignKey(
+        "transaction_types.TransactionType",
+        on_delete=models.CASCADE,
+        related_name="categories",
+    )
+    user = models.ForeignKey(
+        "users.CustomUser",
+        on_delete=models.CASCADE,
+        related_name="categories",
+        null=True,
+    )
+
+    class Meta:
+        db_table = "categories"
+        ordering = ("-created_at",)
