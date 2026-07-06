ae91cdaa9fa336f3fe9b0ee4746ccefeb6cc9fe9
nikrenxd <nikrenc1101@gmail.com>
2026-07-06 23:35:01 +0300

    create permission to check if object is default or belong to authenticated user

--- cash_flow/common/permissions.py
+++ cash_flow/common/permissions.py
@@ -1,7 +1,17 @@
+from rest_framework import permissions
 from rest_framework.permissions import BasePermission
 from rest_framework.request import Request
 
 
 class IsOwnerPermission(BasePermission):
     def has_object_permission(self, request: Request, view, obj) -> bool:
+        return obj.user == request.user
+
+
+class IsOwnerOrDefaultObjectPermission(BasePermission):
+    def has_object_permission(self, request: Request, view, obj) -> bool:
+        # if default object
+        if obj.user is None and request.method in permissions.SAFE_METHODS:
+            return True
+
         return obj.user == request.user
