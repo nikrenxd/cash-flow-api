9666e623db88c2e6af61cfea17e819b1bdfcdb3b
nikrenxd <nikrenc1101@gmail.com>
2026-09-03 16:25:33 +0300

    new fixtures and api client fixture renamed

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,10 @@
-from collections.abc import Generator
-
 import pytest
 from pytest_factoryboy import register
 from rest_framework.test import APIClient
 
+from cash_flow.apps.categories.models import Category
 from cash_flow.apps.statuses.models import Status
+from cash_flow.apps.transaction_types.models import TransactionType
 from tests import constants
 from tests.factories import (
     CategoryFactory,
@@ -31,9 +31,16 @@ def api_client() -> APIClient:
 
 
 @pytest.fixture
-def authorized_client(api_client, custom_user) -> Generator[APIClient]:
+def authenticated_client(api_client, custom_user) -> APIClient:
     api_client.force_login(custom_user)
-    yield api_client
+    return api_client
+
+
+@pytest.fixture
+def other_authenticated_client(api_client, custom_user_factory) -> APIClient:
+    user = custom_user_factory()
+    api_client.force_login(user)
+    return api_client
 
 
 @pytest.fixture
@@ -41,5 +48,22 @@ def default_status(status_factory) -> Status:
     return status_factory(
         name=constants.DEFAULT_STATUS,
         description=constants.DEFAULT_STATUS,
+        user=None,
+    )
+
+
+@pytest.fixture
+def default_transaction_type(transaction_type_factory) -> TransactionType:
+    return transaction_type_factory(
+        name=constants.DEFAULT_TRANSACTION_TYPE,
+        user=None,
+    )
+
+
+@pytest.fixture
+def default_category(category_factory, default_transaction_type) -> Category:
+    return category_factory(
+        name=constants.DEFAULT_CATEGORY,
+        transaction_type=default_transaction_type,
         user=None,
     )
