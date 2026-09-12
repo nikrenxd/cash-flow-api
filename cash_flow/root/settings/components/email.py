a5bd253f54f190c4703a4924b3f697db1a2bf4cd
nikrenxd <nikrenc1101@gmail.com>
2026-09-12 15:41:32 +0300

    add activation email id constants to email settings

--- cash_flow/root/settings/components/email.py
+++ cash_flow/root/settings/components/email.py
@@ -2,8 +2,11 @@ import os
 
 EMAIL_HOST = os.environ.get("EMAIL_HOST")
 EMAIL_PORT = os.environ.get("EMAIL_PORT")
-EMAIL_USE_TLS = bool(os.environ.get("EMAIL_USE_TLS"))
+EMAIL_USE_TLS = bool(int(os.environ.get("EMAIL_USE_TLS")))
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
 DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
+
+ACTIVATION_EMAIL_ID_PREFIX = os.environ.get("ACTIVATION_EMAIL_ID_PREFIX")
+ACTIVATION_EMAIL_ID_TTL = int(os.environ.get("ACTIVATION_EMAIL_ID_TTL"))
